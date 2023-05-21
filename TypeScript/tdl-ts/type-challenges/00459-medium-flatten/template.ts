8ac866ce5fd5c2912791255aefdbd23e15b0ea1d
stevenaces <3313899200@qq.com>
2023-05-21 19:59:20 +0800

    update(TS): ArrFlatten

--- TypeScript/tdl-ts/type-challenges/00459-medium-flatten/template.ts
+++ TypeScript/tdl-ts/type-challenges/00459-medium-flatten/template.ts
@@ -1,10 +1,16 @@
-type Flatten<Arr extends unknown[], Res extends any[] = []> = Arr extends [
-	infer F,
-	...infer R
-]
-	? F extends unknown[]
-		? Flatten<[...F, ...R], Res>
-		: Flatten<[...R], [...Res, F]>
-	: Res;
+// type Flatten<Arr extends unknown[], Res extends any[] = []> = Arr extends [
+// 	infer F,
+// 	...infer R
+// ]
+// 	? F extends unknown[]
+// 		? Flatten<[...F, ...R], Res>
+// 		: Flatten<[...R], [...Res, F]>
+// 	: Res;
+
+type Flatten<T> = T extends []
+	? []
+	: T extends [infer First, ...infer Rest]
+	? [...Flatten<First>, ...Flatten<Rest>]
+	: [T];
 
 export { Flatten };
