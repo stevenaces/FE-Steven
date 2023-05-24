be3caa52859366ea293e93b0b9cd7d7046284e29
stevenaces <3313899200@qq.com>
2023-05-24 17:12:58 +0800

    update(TS): tdl-ts

--- TypeScript/tdl-ts/type-challenges/00612-medium-kebabcase/template.ts
+++ TypeScript/tdl-ts/type-challenges/00612-medium-kebabcase/template.ts
@@ -1,36 +1,39 @@
-type UpperLetter =
-	| "A"
-	| "B"
-	| "C"
-	| "D"
-	| "E"
-	| "F"
-	| "G"
-	| "H"
-	| "I"
-	| "J"
-	| "K"
-	| "L"
-	| "M"
-	| "N"
-	| "O"
-	| "P"
-	| "Q"
-	| "R"
-	| "S"
-	| "T"
-	| "U"
-	| "V"
-	| "W"
-	| "X"
-	| "Y"
-	| "Z";
+// 26个字母转联合类型时的硬编码
+// type UpperLetter =
+// 	| "A"
+// 	| "B"
+// 	| "C"
+// 	| "D"
+// 	| "E"
+// 	| "F"
+// 	| "G"
+// 	| "H"
+// 	| "I"
+// 	| "J"
+// 	| "K"
+// 	| "L"
+// 	| "M"
+// 	| "N"
+// 	| "O"
+// 	| "P"
+// 	| "Q"
+// 	| "R"
+// 	| "S"
+// 	| "T"
+// 	| "U"
+// 	| "V"
+// 	| "W"
+// 	| "X"
+// 	| "Y"
+// 	| "Z";
 
+// 写个工具类将字符串转联合类型
 type StringToUnion<S> = S extends `${infer F}${infer R}`
 	? F | StringToUnion<R>
 	: never;
-type res = StringToUnion<"ABC">;
+type UpperLetter = StringToUnion<"ABCDEFGHIJKLMNOPQRSTUVWXYZ">;
 
+// 方法一
 type KebabCase<S, C extends boolean = false> = S extends `${infer F}${infer R}`
 	? F extends UpperLetter
 		? C extends false
@@ -39,4 +42,10 @@ type KebabCase<S, C extends boolean = false> = S extends `${infer F}${infer R}`
 		: `${F}${KebabCase<R, true>}`
 	: S;
 
+// 方法二
+// type KebabCase<S extends string> = S extends `${infer R}${infer P}`
+// 	? P extends Uncapitalize<P>
+// 		? `${Lowercase<R>}${KebabCase<P>}`
+// 		: `${Lowercase<R>}-${KebabCase<P>}`
+// 	: S;
 export { KebabCase };
