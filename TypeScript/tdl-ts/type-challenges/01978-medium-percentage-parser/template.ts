336d042f0e4328b1973c5e27a9d1f8a2852138aa
stevenaces <3313899200@qq.com>
2023-05-31 14:51:49 +0800

    update(TS): tdl-ts

--- TypeScript/tdl-ts/type-challenges/01978-medium-percentage-parser/template.ts
+++ TypeScript/tdl-ts/type-challenges/01978-medium-percentage-parser/template.ts
@@ -1,16 +1,37 @@
-type PercentageParser<A extends string> = A extends ""
-	? ["", "", ""]
-	: A extends `+${infer Num}%`
-	? ["+", Num, "%"]
-	: A extends `+${infer Num}`
-	? ["+", Num, ""]
-	: A extends `-${infer Num}%`
-	? ["-", Num, "%"]
-	: A extends `-${infer Num}`
-	? ["-", Num, ""]
-	: A extends `${infer Num}%`
-	? ["", Num, "%"]
-	: A extends `${infer Num}`
-	? ["", Num, ""]
-	: never;
+// type PercentageParser<A extends string> = A extends ""
+// 	? ["", "", ""]
+// 	: A extends `+${infer Num}%`
+// 	? ["+", Num, "%"]
+// 	: A extends `+${infer Num}`
+// 	? ["+", Num, ""]
+// 	: A extends `-${infer Num}%`
+// 	? ["-", Num, "%"]
+// 	: A extends `-${infer Num}`
+// 	? ["-", Num, ""]
+// 	: A extends `${infer Num}%`
+// 	? ["", Num, "%"]
+// 	: A extends `${infer Num}`
+// 	? ["", Num, ""]
+// 	: never;
+
+// type PercentageParser<A extends string> = A extends `${infer L}${infer R}`
+// 	? L extends "+" | "-"
+// 		? R extends `${infer Num}%`
+// 			? [L, Num, "%"]
+// 			: [L, R, ""]
+// 		: A extends `${infer Num}%`
+// 		? ["", Num, "%"]
+// 		: ["", A, ""]
+// 	: ["", "", ""];
+
+// 封装工具类，使得逻辑更加清晰
+type CheckPrefix<T> = T extends "+" | "-" ? T : never;
+type CheckSuffix<T> = T extends `${infer Num}%` ? [Num, "%"] : [T, ""];
+
+type PercentageParser<A extends string> = A extends `${CheckPrefix<
+	infer L
+>}${infer R}`
+	? [L, ...CheckSuffix<R>]
+	: ["", ...CheckSuffix<A>];
+
 export { PercentageParser };
