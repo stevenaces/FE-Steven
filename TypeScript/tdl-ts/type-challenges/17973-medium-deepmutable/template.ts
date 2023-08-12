ac1f947324eabb35b99a9b6fa801fbf929e00428
stevenaces <3313899200@qq.com>
2023-08-12 19:26:47 +0800

    update(TS): tdl-ts

--- TypeScript/tdl-ts/type-challenges/17973-medium-deepmutable/template.ts
+++ TypeScript/tdl-ts/type-challenges/17973-medium-deepmutable/template.ts
@@ -1,10 +1,18 @@
-type DeepMutable<T extends Record<string, any>> = {
-  -readonly [K in keyof T]:
-  T[K] extends object
-  ? T[K] extends Function
-  ? T[K]
-  : DeepMutable<T[K]>
-  : T[K]
-}
+// my solution
+// type DeepMutable<T extends Record<string, any>> = {
+//   -readonly [K in keyof T]:
+//   T[K] extends object
+//   ? T[K] extends Function
+//   ? T[K]
+//   : DeepMutable<T[K]>
+//   : T[K]
+// }
 
+// other solution
+type DeepMutable<T extends Record<string, any>> =
+  T extends Function
+  ? T
+  : {
+    - readonly [K in keyof T]: DeepMutable<T[K]>
+  }
 export { DeepMutable };
